ffb231d11efd0808e14b24317e30c789a8d7b259
niharikav27 <neerjav80@gmail.com>
2026-06-04 10:18:24 +0530

    optimal solution- merge sort

---
+++ DAY 4/count_inversions2.cpp
@@ -0,0 +1,47 @@
+class Solution {
+private:
+       long long int merge(vector<int>& arr, int low, int mid, int high) {
+        vector<int> temp;
+        int left = low;
+        int right = mid + 1;
+        long long int cnt = 0;
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.push_back(arr[left]);
+                left++;                
+            } 
+            else {
+                temp.push_back(arr[right]);
+                cnt += (mid - left + 1);
+                right++;
+            }
+        }
+        while (left <= mid) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            temp.push_back(arr[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp[i - low];
+        }
+        return cnt;
+    }
+    long long int mergeSort(vector<int>& arr, int low, int high) {
+        long long int cnt = 0;
+        if (low < high) {
+            int mid = low + (high - low) / 2;
+            cnt += mergeSort(arr, low, mid);  
+            cnt += mergeSort(arr, mid + 1, high); 
+            cnt += merge(arr, low, mid, high);  
+        }
+        return cnt;
+    }
+public:
+    long long int numberOfInversions(vector<int>& nums) {
+        int n = nums.size();
+        return mergeSort(nums, 0, n - 1);
+    }
+};
