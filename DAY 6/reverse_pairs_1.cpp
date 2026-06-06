f82aaa4142eb9a7999c211c9ec8605f5cb091696
niharikav27 <neerjav80@gmail.com>
2026-06-06 09:33:28 +0530

    brute force

---
+++ DAY 6/reverse_pairs_1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        int n = nums.size();
+        int cnt = 0;
+        for(int i = 0 ; i<n ; i++){
+            for(int j = i+1 ; j<n ; j++){
+                if(nums[i] > 2*nums[j]){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
