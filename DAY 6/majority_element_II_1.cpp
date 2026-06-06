64197b5575b908895180f72fdaa652968f2f8087
niharikav27 <neerjav80@gmail.com>
2026-06-06 09:31:48 +0530

    brute force

---
+++ DAY 6/majority_element_II_1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> majorityElementTwo(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans;
+        for(int i = 0 ; i<n ;i++){
+            if(ans.size()  == 0 || ans[0] != nums[i]){
+                int cnt = 0;
+                for(int j = 0 ; j<n ; j++){
+                    if(nums[i] == nums[j]){
+                        cnt++;
+                    }
+                }
+                if(cnt > (n/3)){
+                    ans.push_back(nums[i]);
+                }
+            }
+            if(ans.size() == 2) break;
+        }
+        return ans;
+    }
+};
