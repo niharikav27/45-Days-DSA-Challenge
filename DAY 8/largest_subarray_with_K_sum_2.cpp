7b0817c81397b326949b8b1b4ca304fb7df4e6b3
niharikav27 <neerjav80@gmail.com>
2026-06-08 09:30:57 +0530

    better solution

---
+++ DAY 8/largest_subarray_with_K_sum_2.cpp
@@ -0,0 +1,24 @@
+class Solution{
+public:
+    int longestSubarray(vector<int> &nums, int k){
+        int n = nums.size();
+        int sum = 0 ;
+        int maxLen = 0;
+        map<int,int>preSumMap;
+        for(int i = 0 ; i<n ; i++){
+            sum += nums[i];
+            if(sum == k){
+                maxLen = max(maxLen , i+1);
+            }
+            int rem = sum - k ;
+            if(preSumMap.find(rem) != preSumMap.end()){
+                int len = i - preSumMap[rem];
+                maxLen = max(maxLen , len);
+            }
+            if(preSumMap.find(sum) == preSumMap.end()){
+                preSumMap[sum] = i;
+            }
+        }
+        return maxLen;
+    }
+};
