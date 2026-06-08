b880a3614949b6c732f2b657e578b3b0326a5ff3
niharikav27 <neerjav80@gmail.com>
2026-06-08 09:29:25 +0530

    brute force

---
+++ DAY 8/count_subarrays_with_XOR_K_1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int subarraysWithXorK(vector<int>& nums, int k) {
+        int n = nums.size();
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+
+                int xorr = 0;
+
+                for (int l = i; l <= j; l++) {
+                    xorr ^= nums[l];
+                }
+
+                if (xorr == k) {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
