4db7636e957370ffd3e6ba9a7f254aa23f0a20bb
niharikav27 <neerjav80@gmail.com>
2026-06-25 10:35:15 +0530

    optimal solution

---
+++ DAY 25/balanced_parenthesis.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        int n = str.size();
+        stack<char>st;
+        for(int i = 0 ; i<n ; i++){
+            if(str[i] == '(' || str[i] == '[' || str[i] == '{'){
+                st.push(str[i]);
+            }
+            else{
+                if(st.empty()) return false;
+                char ch = st.top();
+                st.pop();
+                if((str[i] == ')' && ch =='(') ||
+                    (str[i] == ']' && ch == '[') ||
+                    (str[i] == '}' && ch == '{')){
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
