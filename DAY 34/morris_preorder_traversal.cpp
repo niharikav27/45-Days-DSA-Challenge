ec9c2b990ccc488882bac606683c426b55526745
niharikav27 <neerjav80@gmail.com>
2026-07-04 12:01:01 +0530

    optimal solution

---
+++ DAY 34/morris_preorder_traversal.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
+class Solution{
+	public:
+		vector<int> preorder(TreeNode* root){
+	       vector<int> preorder;
+           TreeNode* cur = root;
+           while (cur != NULL) {
+                if (cur->left == NULL) {
+                    preorder.push_back(cur->data);
+                    cur = cur->right;
+                }      
+                else {
+                    TreeNode* prev = cur->left;
+                    while (prev->right && prev->right != cur) {
+                        prev = prev->right;
+                    }
+                    if (prev->right == NULL) {
+                        prev->right = cur;
+                        preorder.push_back(cur->data);
+                        cur = cur->left;
+                    }
+                    else {
+                        prev->right = NULL;
+                        cur = cur->right;
+                    }
+                }
+            }
+            return preorder;
+		}
+};
