c755530066d717f14066055682838fe5bcf12217
niharikav27 <neerjav80@gmail.com>
2026-07-08 10:29:20 +0530

    optimal solution

---
+++ DAY 38/max_path_sum.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
+class Solution {	
+public:
+    int maxPathSum(TreeNode* root) {
+        int maxi = INT_MIN;
+        maxPathDown(root , maxi);
+        return maxi;
+    }
+    int maxPathDown(TreeNode* node , int &maxi){
+        if(node == NULL) return 0;
+        int left = max(0 , maxPathDown(node->left,maxi));
+        int right = max(0 , maxPathDown(node->right,maxi));
+        maxi = max(maxi, left+right+node->data);
+        return max(left,right) + node -> data;
+    }
+};	
