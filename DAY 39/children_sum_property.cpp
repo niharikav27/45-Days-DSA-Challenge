5e81d94a2f158c2f620ec7dab9ebaf3a2de37fd0
niharikav27 <neerjav80@gmail.com>
2026-07-09 13:53:13 +0530

    optimal solution

---
+++ DAY 39/children_sum_property.cpp
@@ -0,0 +1,21 @@
+/* class TreeNode {
+       int val;
+       TreeNode *left, *right;
+       TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+   };
+*/
+
+class Solution {
+public:
+    bool checkChildrenSum(TreeNode* root) {
+        if (root == NULL) return true;
+        if (!root->left && !root->right) return true;
+
+        int leftVal = root->left ? root->left->val : 0;
+        int rightVal = root->right ? root->right->val : 0;
+
+        return (root->val == leftVal + rightVal) &&
+               checkChildrenSum(root->left) &&
+               checkChildrenSum(root->right);
+    }
+};
