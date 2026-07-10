01cc7b21d8b2e2be7f94153cdbb1ac927fc52771
niharikav27 <neerjav80@gmail.com>
2026-07-10 14:31:08 +0530

    optimal solution

---
+++ DAY 40/search_in_bst.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
+class Solution {	
+public:	
+    TreeNode* searchBST(TreeNode* root, int val) {
+    	while (root != nullptr && root->data != val) {
+            root = (root->data > val) ? root->left : root->right;
+        }
+        return root;
+    }
+};
