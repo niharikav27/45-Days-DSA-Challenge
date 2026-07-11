08411c4115147a3f120806a5b8c7a106fb768421
niharikav27 <neerjav80@gmail.com>
2026-07-11 12:48:13 +0530

    optimal solution

---
+++ DAY 41/bst_from_preorder.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i = 0;
+        return build(preorder,i,INT_MAX);
+    }
+    TreeNode* build(vector<int>&preorder , int &i , int bound){
+        if(i == preorder.size() || preorder[i] > bound) return NULL;
+        TreeNode* root = new TreeNode(preorder[i++]);
+        root->left = build(preorder,i,root->data);
+        root->right = build(preorder,i,bound);
+        return root;
+    }
+};
