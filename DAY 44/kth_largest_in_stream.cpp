c3d5fb259d1d932e69e1b649ae0fda881ed180a0
niharikav27 <neerjav80@gmail.com>
2026-07-14 10:51:40 +0530

    optimal solution

---
+++ DAY 44/kth_largest_in_stream.cpp
@@ -0,0 +1,26 @@
+class KthLargest {
+public:
+    int K; 
+    priority_queue <int, vector<int>, greater<int>> pq;
+    KthLargest(int k, vector<int>& nums) {
+        K = k; 
+        for(int i = 0; i < nums.size(); i++) {
+            if(pq.size() < K) pq.push(nums[i]);
+            else if(nums[i] > pq.top()) {
+                pq.pop(); 
+                pq.push(nums[i]); 
+            }
+        }
+    }
+    int add(int val) {
+        if(pq.size() < K) {
+            pq.push(val);
+            return pq.top();
+        }
+        if(val > pq.top()) {
+            pq.pop();
+            pq.push(val); 
+        }
+        return pq.top();
+    }
+};
